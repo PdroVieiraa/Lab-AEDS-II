93ce4c0fc9bcbdd741b8b93d0d622460eb82ada9
PdroVieiraa <pedrohsantos355@gmail.com>
2026-08-18 17:56:14 -0300

    Finalização da classe de produtos perecíveis

--- aeds2/src/ProdutoNaoPerecivel.java
+++ aeds2/src/ProdutoNaoPerecivel.java
@@ -1,16 +1,62 @@
-public class ProdutoNaoPerecivel extends Produto{
-    
-    public ProdutoNaoPerecivel (String desc, double precoCusto,double margemLucro){
-        super(desc, precoCusto, margemLucro);
+import java.time.LocalDate;
+
+public class ProdutoPerecivel extends Produto {
+
+    // Desconto de 25% aplicado aos produtos próximos do vencimento
+    private static final double DESCONTO = 0.25;
+
+    // Quantidade de dias para considerar o produto próximo do vencimento
+    private static final int PRAZO_DESCONTO = 7;
+
+    // Data de validade do produto
+    private LocalDate dataDeValidade;
+
+
+    public ProdutoPerecivel(String desc, double precoCusto, LocalDate dataDeValidade) {
+        super(desc, precoCusto);
+
+        // Não permite cadastrar um produto que já esteja vencido
+        if (dataDeValidade.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException(
+                "A data de validade não pode ser anterior à data atual."
+            );
+        }
+
+        this.dataDeValidade = dataDeValidade;
     }
 
-    public ProdutoNaoPerecivel(String desc, double precoCusto) {
-        super(desc, precoCusto);
-    }
 
     @Override
     public double valorDeVenda() {
-        return precoCusto * (1 + margemLucro);
-    }      
 
+        LocalDate hoje = LocalDate.now();
+
+        // Verifica novamente a validade no momento da venda,
+        // pois o produto pode ter vencido depois de ter sido cadastrado
+        if (dataDeValidade.isBefore(hoje)) {
+            throw new IllegalStateException(
+                "Não é possível vender um produto vencido."
+            );
+        }
+
+        // Utiliza o cálculo normal de venda definido na classe Produto
+        double valor = super.valorDeVenda();
+
+        // Se o produto vence em até 7 dias, aplica desconto de 25%
+        if (!dataDeValidade.isAfter(hoje.plusDays(PRAZO_DESCONTO))) {
+            valor = valor * (1 - DESCONTO);
+        }
+
+        return valor;
+    }
+
+
+    @Override
+    public String toString() {
+
+        // Acrescenta a data de validade às informações do produto
+        return super.toString()
+                + "\nData de validade: "
+                + dataDeValidade;
+    }
 }
